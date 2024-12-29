c5380e7cda23875ee17331a7127212c7c1be8c94
kyambuthia <kyambuthia@gmail.com>
2024-12-29 23:59:49 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/kyambuthia/sokomoko
+
+go 1.23.3
+
+require github.com/ncruces/go-sqlite3 v0.21.3
+
+require (
+	github.com/ncruces/julianday v1.0.0 // indirect
+	github.com/tetratelabs/wazero v1.8.2 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+)
---
+++ src/srvr.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+        "fmt"
+        "log"
+        "database/sql"
+
+        _ "github.com/ncruces/go-sqlite3/driver"
+        _ "github.com/ncruces/go-sqlite3/embed"
+)
+
+type Product struct {
+        name string     `json:"name"`
+        price float64   `json:"price"`
+        category string `json:"category"`
+}
+
+func main() {
+        db, err := sql.Open("sqlite3", "./db/t.db")
+        if (err != nil) {
+                log.Fatal(err)
+        }
+        defer db.Close()
+
+        var prod = Product{"Banana Cake", 300, "cake"}
+        fmt.Println(prod)
+
+        // Create DB
+
+
+        err = db.QueryRow(`SELECT * FROM names`).Scan(&result, &phonenumber)
+        if err != nil {
+                log.Fatal(err)
+        }
+}
