f4df7344616dfddba525a4ea818a10a4dff3005f
kyambuthia <kyambuthia@gmail.com>
2025-05-17 08:32:43 +0300

    TODO: add webcomponents for the product cards.

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -1,1 +1,20 @@
-console.log("-->");
+if (document.readyState === "complete")
+{
+    console.log("Document is ready");
+    console.log("-->");
+
+    const currentTime = Date.now();
+
+    const bannerArea = document.createElement("div");
+    bannerArea.className="banner-area";
+    bannerArea.innerHTML = currentTime;
+
+    const siteHeader = document.querySelector("header");
+    siteHeader.appendChild(bannerArea);
+}
+
+document.addEventListener("DOMContentLoaded", () => 
+{
+    console.log("ready");
+});
+
