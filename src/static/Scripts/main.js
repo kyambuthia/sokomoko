ffe4979349b6fe8f7108dbbb693984ba17b9d1d9
kyambuthia <kyambuthia@gmail.com>
2025-05-23 11:07:04 +0300

    JS Script Files

---
+++ src/static/Scripts/main.js
@@ -0,0 +1,24 @@
+() =>
+{
+    if (document.readyState === "complete")
+    {
+        console.log("Document is ready");
+        console.log("-->");
+
+        const currentTime = Date.now();
+
+        const bannerArea = document.createElement("div");
+        bannerArea.className="banner-area";
+        bannerArea.innerHTML = currentTime;
+
+        const siteHeader = document.querySelector("header");
+        siteHeader.appendChild(bannerArea);
+    }
+
+    document.addEventListener("DOMContentLoaded", () => 
+    {
+        console.log("ready");
+    });
+}
+
+
