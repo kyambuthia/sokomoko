c066b940240eafd8aea660f2bdc13c781a00d2c7
kyambuthia <kyambuthia@gmail.com>
2025-06-12 22:42:32 +0300

    will move routes into a separate directory.

--- src/main.go
+++ src/main.go
@@ -15,12 +15,28 @@ var tmplData embed.FS
 func main() {
 	mux := http.NewServeMux()
 
+	// Static file handler - CSS, JS, IMGS.
+	staticFS := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/", http.StripPrefix("/static/", staticFS))
+
+	// Root Route Handler.
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		tmpl, err := template.ParseFS(tmplData, "templates/indx.html") ; if (err != nil) {
+		tmpl, err := template.ParseFS(
+			tmplData, 
+			"templates/layout.html",
+			"templates/index.html",
+			"templates/components/header.html",
+			"templates/components/nav.html",
+			"templates/components/footer.html",
+		)
+
+		if (err != nil) {
 			log.Fatal(err)
 		}
 
-		tmpl.ExecuteTemplate(w, "root_template", "habari dunia")
+		err = tmpl.ExecuteTemplate(w, "root_template", "habari dunia") ; if (err != nil) {
+			log.Fatal(err)
+		}
 	})
 	
 	srvr := &http.Server {
@@ -30,10 +46,8 @@ func main() {
 		WriteTimeout: 15 * time.Second,
 	}
 
-	err := srvr.ListenAndServe()
-	fmt.Printf("server is running on https://localhost/$s", srvr.Addr)
-
-	if (err != nil) {
+	fmt.Printf("server is running on PORT %s \nCTRL-C to EXIT\n", srvr.Addr)
+	err := srvr.ListenAndServe() ; if (err != nil) {
 		log.Fatal(err)
 	}
 }
