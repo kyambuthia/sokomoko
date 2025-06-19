54fb2e4f9304858187a935268a28c71d52eee975
kyambuthia <kyambuthia@gmail.com>
2025-06-19 22:21:24 +0300

    updated the JS scripts.

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -1,24 +1,24 @@
-console.log("Habari, dunia")
+window.onload = () =>
+{
+    const searchForm = document.querySelector("#searchForm")
 
-const searchForm = document.querySelector("#searchForm")
+    searchForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const data = {
+            searchQuery: form.value.queryString
+        }
 
-searchForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const data = {
-        searchQuery: form.value.queryString
-    }
+        const res = await fetch("/search", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
 
-    const res = await fetch("/search", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
+        const text = await res.text();
+        console.log(text)
+
     });
-
-    const text = await res.text();
-    console.log(text)
-
-});
-
+}
