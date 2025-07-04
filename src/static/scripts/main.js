f1709f0faeb4a01c62e4bc2665ccecf59f878d9c
kyambuthia <kyambuthia@gmail.com>
2025-07-04 11:41:57 +0300

    TODO: separate scripts by route.

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -1,14 +1,18 @@
 window.onload = () =>
 {
-    const searchForm = document.querySelector("#searchForm")
-
+    const searchForm = document.querySelector("#searchForm");
     searchForm.addEventListener("submit", async (e) => {
         e.preventDefault();
-        const form = e.target;
+
+        const formData = new FormData(searchForm);
+        // DEBUGS 
+        //  ---
+        //console.log(formData.get("queryString"));
         const data = {
-            searchQuery: form.value.queryString
+            queryString: formData.get("queryString")
         }
 
+        //console.log(JSON.stringify(data));
         const res = await fetch("/search", {
             method: "POST",
             headers: {
@@ -18,7 +22,29 @@ window.onload = () =>
         });
 
         const text = await res.text();
-        console.log(text)
-
+        console.log("response text: \n", text)
     });
+
+    // read values from the search-form.
+    async function readSearchFormValues()
+    {
+        const url = "/search";
+        try
+        {
+            const res =  await fetch(url)
+            if (!res.ok)
+            {
+                throw Error(`RESPONSE: ${res.status}`)
+            }
+            const content = await res.json();
+            console.log(json);
+        }
+        catch(err)
+        {
+            console.error(err.message);
+        }
+    }
+
+    // --> load JSON from server.
+    readSearchFormValues();
 }
