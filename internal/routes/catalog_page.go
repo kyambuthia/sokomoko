02fd9a418ccc142d3c4d5a0ff9b13cd51b872e32
kyambuthia <kyambuthia@gmail.com>
2026-04-09 21:22:53 +0300

    Add catalog titles

--- internal/routes/catalog_page.go
+++ internal/routes/catalog_page.go
@@ -3,11 +3,13 @@ package routes
 import catalogsvc "github.com/kyambuthia/sokomoko/internal/service/catalog"
 
 type IndexPageData struct {
+	Title      string
 	HasProducts bool
 	Products    []catalogsvc.Product
 }
 
 type SearchPageData struct {
+	Title    string
 	Query     string
 	Products  []catalogsvc.Product
 	NoResults bool
@@ -15,6 +17,7 @@ type SearchPageData struct {
 
 func indexPage(products []catalogsvc.Product) IndexPageData {
 	return IndexPageData{
+		Title:      "Home",
 		HasProducts: len(products) > 0,
 		Products:    products,
 	}
@@ -29,6 +32,7 @@ func productPage(product *catalogsvc.Product) ProductPageData {
 
 func searchPage(query string, products []catalogsvc.Product) SearchPageData {
 	return SearchPageData{
+		Title:    "Search",
 		Query:     query,
 		Products:  products,
 		NoResults: len(products) == 0 && query != "",
